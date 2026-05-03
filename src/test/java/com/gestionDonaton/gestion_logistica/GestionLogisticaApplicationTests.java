88b8f64fe0f0cb13a144d5b1b7624ae32679a815
camilo2046M <ca.menav@duocuc.cl>
2026-05-03 13:00:51 -0400

    Initial commit para microservicio gestion logistica de Donaton y su pom.xml base

---
+++ src/test/java/com/gestionDonaton/gestion_logistica/GestionLogisticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionDonaton.gestion_logistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionLogisticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
