cca916b96d96fa37d27c6a24231cda9b890d5a2a
esquizo <ma.lorcam@duocuc.cl>
2026-05-08 18:48:02 -0400

    adicion de dependencias y creacion de base de datos para envios

--- src/main/java/com/gestionDonaton/gestion_logistica/controller/EnvioController.java
+++ src/main/java/com/gestionDonaton/gestion_logistica/controller/EnvioController.java
@@ -1,10 +1,12 @@
 package com.gestionDonaton.gestion_logistica.controller;
 
 
+import com.gestionDonaton.gestion_logistica.client.DonacionClient;
 import com.gestionDonaton.gestion_logistica.dto.EnvioRequestDTO;
 import com.gestionDonaton.gestion_logistica.dto.EnvioResponseDTO;
 import com.gestionDonaton.gestion_logistica.service.EnvioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/envios")
+@RequestMapping("/api/envios")
 @RequiredArgsConstructor
 public class EnvioController {
 
@@ -27,5 +29,18 @@ public class EnvioController {
     @GetMapping
     public ResponseEntity<List<EnvioResponseDTO>> listarEnvios() {
         return ResponseEntity.ok(envioService.listarEnvios());
+    }
+
+    @Autowired
+    private DonacionClient donacionClient; // El que acabamos de crear
+
+    @GetMapping("/test-conexion/{palabra}")
+    public ResponseEntity<?> probarConexion(@PathVariable String palabra) {
+        try {
+            // Llamamos al otro microservicio a través de Feign
+            return ResponseEntity.ok(donacionClient.buscarPorPalabra(palabra));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Error conectando a Donaciones: " + e.getMessage());
+        }
     }
 }
