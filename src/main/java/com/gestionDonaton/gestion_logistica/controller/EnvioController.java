ad0cfb5695feb0b0b13214bb728aba0548114266
camilo2046M <ca.menav@duocuc.cl>
2026-06-16 22:58:20 -0400

    Modificado controlador de envío

--- src/main/java/com/gestionDonaton/gestion_logistica/controller/EnvioController.java
+++ src/main/java/com/gestionDonaton/gestion_logistica/controller/EnvioController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/envios")
 @RequiredArgsConstructor
-public class EnvioController {
+public class    EnvioController {
 
     private final EnvioService envioService;
 
